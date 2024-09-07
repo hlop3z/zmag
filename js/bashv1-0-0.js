0372db792d79a324ed937a1367b7c23da1028d4c
hlop3z <23062270+hlop3z@users.noreply.github.com>
2024-09-06 19:55:37 -0500

    Deployed f5d4be2 with MkDocs version: 1.6.1

--- js/bashv1-0-0.js
+++
@@ -1,5 +0,0 @@
-terminal("#terminal-index", [
-  { type: "input", value: 'python -m pip install "zmag[debug]"' },
-  { type: "progress", value: 100 },
-  { value: "Successfully installed zmag", startDelay: 0 },
-]);
